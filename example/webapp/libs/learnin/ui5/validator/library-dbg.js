8897061bc07d3f115d4237b55180fa61b26e1e1a
learnin <i.learnin@gmail.com>
2021-05-27 00:17:54 +0900

    Up to 0.1.6 for example

--- example/webapp/libs/learnin/ui5/validator/library-dbg.js
+++ example/webapp/libs/learnin/ui5/validator/library-dbg.js
@@ -15,12 +15,12 @@
      *
      * @namespace
      * @name learnin.ui5.validator
-     * @version 0.1.5
+     * @version 0.1.6
      * @public
      */
     return sap.ui.getCore().initLibrary({
         name: "learnin.ui5.validator",
-        version: "0.1.5",
+        version: "0.1.6",
         dependencies: [
             "sap.ui.core",
             "sap.m"
--- example/webapp/libs/learnin/ui5/validator/library.js
+++ example/webapp/libs/learnin/ui5/validator/library.js
@@ -1,4 +1,4 @@
 /*!
  * ${copyright}
  */
-sap.ui.define(["sap/ui/core/library"],()=>{"use strict";return sap.ui.getCore().initLibrary({name:"learnin.ui5.validator",version:"0.1.5",dependencies:["sap.ui.core","sap.m"],types:[],interfaces:[],controls:["learnin.ui5.validator.Validator"],elements:[],noLibraryCSS:true})});
+sap.ui.define(["sap/ui/core/library"],()=>{"use strict";return sap.ui.getCore().initLibrary({name:"learnin.ui5.validator",version:"0.1.6",dependencies:["sap.ui.core","sap.m"],types:[],interfaces:[],controls:["learnin.ui5.validator.Validator"],elements:[],noLibraryCSS:true})});
